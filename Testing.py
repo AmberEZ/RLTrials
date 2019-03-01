bf2364dbc457b2ae8b85fe70005c5ef7b1ab69a1
AmberEZ <amberzelvelder@gmail.com>
2019-03-01 17:43:02 +0100

    Created 4 test cases
    Test cases for all 4 functions that have outputs

--- Testing.py
+++ Testing.py
@@ -28,24 +28,24 @@ None of them have outputs
 class TestTS(unittest.TestCase):
         """
         functions in TS:
-            TS1Graph(goal, initial_state, MATRIX_SIZE, gamma) - no return values
-                available_actions(self) - takes an integer (state), returns an integer (row)
-                sample_next_action(available_actions_range) - takes an integer (row), returns an integer (action)
-                collect_environmental_data(action) - takes an integer(action), returns a list (found)
-                update(current_state, action, gamma) - takes an integer (state), another integer (action) and a decimal number (gamma, the learning parameter)
-                available_actions_with_enviro_help(state) - takes an integer (state) and returns an integer (row)
+           *available_actions(self)
+           *sample_next_action(available_actions_range) - takes an integer (row), returns an integer (action)
+           *collect_environmental_data(action) - takes an integer(action), returns a list (found) 
+           *available_actions_with_enviro_help(state) - takes an integer (state) and returns an integer (row)
+           TS1Graph(goal, initial_state, MATRIX_SIZE, gamma) - no return values
             
         """
         def test_available_actions(self): #takes an integer (state) and matrix(R), returns an array
             npt.assert_array_equal(TS.available_actions(1, np.matrix([[1, 2], [3, 4]]) ), [0, 1] )
             
-#        def test_sample_next_action(available_actions_range):
-#            
-#        def test_collect_environmental_data(action):
-#            
-#        def test_update(current_state, action, gamma):
-#            
-#        def test_available_actions_with_enviro_help(state):
+        def test_sample_next_action(self):
+            self.assertIsInstance(TS.sample_next_action([1,2]), int)
+                        
+        def test_collect_environmental_data(self): #takes an int and two arrays and returns an array
+            self.assertAlmostEquals(TS.collect_environmental_data(1,[2],[3]), [])
+           
+        def test_available_actions_with_enviro_help(self): # takes a state and two matrices and returns an array
+            npt.assert_array_equal(TS.available_actions_with_enviro_help(1, np.matrix([[1, 2], [3, 4]]), np.matrix([[1, 2], [3, 4]])), [0, 1])
 
 
 if __name__ == '__main__':
